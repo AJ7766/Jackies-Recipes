6b838d8ea8d9768140a8a7be70f4dfe3f83b6c97
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-07-01 22:13:09 +0200

    km

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script>0</script>
+      </head>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import LoginForm from "./_components/LoginForm";
 
 export default function Home() {
+
   return (
     <div className="w-screen h-screen bg-home-background bg-cover bg-center">
       <LoginForm />
