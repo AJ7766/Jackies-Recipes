c985002dd3400f9249b6cc696e7660c59351b078
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-07-02 12:45:41 +0200

    asd

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -2,7 +2,7 @@ import ForgotPassForm from "./_components/ForgotPassForm";
 
 export default function ForgotPasswordPage() {
   return (
-    <div className="w-screen h-screen bg-home-background bg-center">
+    <div className="bgContainer">
       <ForgotPassForm />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import LoginForm from "./_components/LoginForm";
 export default function Home() {
 
   return (
-    <div className="w-screen h-screen bg-home-background bg-center -z-1">
+    <div className="bgContainer">
       <LoginForm />
     </div>
   );
