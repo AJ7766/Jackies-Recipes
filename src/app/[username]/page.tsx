ef63df16133e7cb9683441edb2781c350f90e620
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-08-13 20:32:16 +0200

    Update page.tsx

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,16 +1,4 @@
 "use client"
-import { useAuth } from "../context/AuthContext";
-
-export default function UserPage({children}: {children: React.ReactNode}) {
-
-  const {initializing} = useAuth();
-
-  if(initializing){
-    return null;
-  }
-  return (
-    <>
-      {children}
-  </>
-  );
+export default function UserPage() {
+  return null
 }
