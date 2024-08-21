ead1f031305f9ec153f598f9b9c921958393acd3
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-08-21 10:50:39 +0200

    Update route.ts

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,9 +1,11 @@
+import { connectDB } from "@/config/database";
 import { UserModel } from "@/models/UserModel";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
     const { username } = await request.json()
     const regex = new RegExp(username, 'i');
+    await connectDB();
     const existingUsers = await UserModel.find({ username: { $regex: regex } }).limit(5).select('-_id username fullName userContent.profilePicture');;
     if (!existingUsers) {
         throw new Error(`No existing user with username ${username}`);
