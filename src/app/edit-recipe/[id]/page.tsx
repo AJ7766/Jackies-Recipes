c1f89761746613ea06bddeb5d4d516470e5b65ea
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-10-04 17:31:43 +0200

    changed component name

--- src/app/edit-recipe/[id]/page.tsx
+++ src/app/edit-recipe/[id]/page.tsx
@@ -4,7 +4,7 @@ import ErrorPage from "@/app/_components/ErrorPage";
 import NavBar from "@/app/_components/NavBar";
 import useFetchRecipe from "../_action/useFetchRecipe";
 import { useAuth } from "@/app/context/AuthContext";
-import EditRecipeForm from "../_components/asd";
+import EditRecipeForm from "../_components/EditRecipeForm";
 
 export default function EditRecipePage({ params }: { params: { id: string } }) {
   const { id } = params;
