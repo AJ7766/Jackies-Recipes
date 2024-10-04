3ea5bbd9802fc54f80a4aa25a38d0d3e22ab2be3
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-10-04 17:24:42 +0200

    Update page.tsx

--- src/app/edit-recipe/[id]/page.tsx
+++ src/app/edit-recipe/[id]/page.tsx
@@ -2,9 +2,9 @@
 
 import ErrorPage from "@/app/_components/ErrorPage";
 import NavBar from "@/app/_components/NavBar";
+import EditRecipeForm from "../_components/EditRecipeForm";
 import useFetchRecipe from "../_action/useFetchRecipe";
 import { useAuth } from "@/app/context/AuthContext";
-import EditRecipeForm from "../_components/EditRecipeForm";
 
 export default function EditRecipePage({ params }: { params: { id: string } }) {
   const { id } = params;
