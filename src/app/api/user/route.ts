bdee1032d7113e40b4be65978707b2913711976f
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-10-04 16:32:25 +0200

    fixed dynamic error

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,16 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { handlePut } from "./handlers/handlePut";
 import { handleGet } from "./handlers/handleGet";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
     try {
-        const authHeader = request.headers.get('Authorization') || request.headers.get('authorization');
-
-        if (!authHeader || !authHeader.startsWith('Bearer')) {
-          return NextResponse.json({ message: 'No header, Unauthorized' }, { status: 401 });
-        }
-        const token = authHeader.split(' ')[1];
-
-        return await handleGet(request, token);
+        return await handleGet(request);
     } catch (error) {
         console.error(error);
         return NextResponse.json({ success: false, message: "Internal Server Error" }, { status: 500 });
