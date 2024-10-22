60947b247a118d8123e83e0cd06bf1b65a78cab4
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-10-22 14:04:48 +0200

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -102,6 +102,11 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-W37LZK4XFJ"
         ></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6350037682611520"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body>
         <AuthProvider>
