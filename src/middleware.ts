7abd474b53f96024e3c68377dad6f8e6727cd9c7
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-05 12:10:22 +0100

    new headers for security

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,8 @@ export function middleware() {
     response.headers.set('X-Content-Type-Options', 'nosniff');
     response.headers.set('X-Frame-Options', 'DENY');
     response.headers.set('X-XSS-Protection', '1; mode=block');
+    response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+    response.headers.set('Cross-Origin-Resource-Policy', 'same-origin');
     response.headers.set(
         'Content-Security-Policy',
         "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data:;"
