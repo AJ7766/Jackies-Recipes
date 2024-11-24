6a8245330f6d7e531238891198d5ac2168b68259
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-24 23:50:40 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
           headers: [
             {
               key: 'Cache-Control',
-              value: 'public, max-age=60, must-revalidate',
+              value: 'public, max-age=0, must-revalidate',
             }
           ],
         },
