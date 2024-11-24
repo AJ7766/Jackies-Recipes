ddb74516aaac42176fd82b66a422f3619ace6718
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-24 22:57:20 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true, // Optional: If you want to disable Next.js image optimization
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, must-revalidate',
-          },
-        ],
-      },
-      {
-        source: '/:all*(svg|jpg|png|webp)',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=60, must-revalidate',
-          },
-        ],
-      },
-    ];
-  },
+   images: {
+      unoptimized: true,
+    },
+   async headers() {
+      return [
+        {
+          source: '/:all*(svg|jpg|png|webp)',
+          locale: false,
+          headers: [
+            {
+              key: 'Cache-Control',
+              value: 'public, max-age=60, must-revalidate',
+            }
+          ],
+        },
+      ]
+    },
 };
 
 export default nextConfig;
