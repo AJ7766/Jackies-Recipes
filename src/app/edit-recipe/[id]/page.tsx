cced12631110d2cc76ba556fdd1b06be0ccea015
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-24 23:29:16 +0100

    Update page.tsx

--- src/app/edit-recipe/[id]/page.tsx
+++ src/app/edit-recipe/[id]/page.tsx
@@ -1,12 +1,12 @@
 import { AuthGuard } from "@/app/_services/authGuard";
 import EditRecipe from "../containers/editRecipe";
 
-export default function EditRecipePage({ params }: { params: { id: string } }) {
-  const { id } = params;
+export default async function EditRecipePage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
 
   return (
     <AuthGuard>
       <EditRecipe recipe_id={id} />
     </AuthGuard>
-    )
+  )
 }
