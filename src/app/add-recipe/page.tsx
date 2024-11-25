62d081fb0aad54e3a2e26ee974ec5a77a68efe83
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-25 20:48:45 +0100

    Update page.tsx

--- src/app/add-recipe/page.tsx
+++ src/app/add-recipe/page.tsx
@@ -1,6 +1,8 @@
 import { AuthGuard } from "../_services/authGuard";
 import AddRecipe from "./containers/AddRecipe";
 
+export const dynamic = 'force-static';
+
 export default function AddRecipePage() {
 
   return (
