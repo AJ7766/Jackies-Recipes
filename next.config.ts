b1b727a43a5db147d1659d4cfccc565674a17399
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-28 22:06:59 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,19 @@
 module.exports = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "res.cloudinary.com",
-        pathname: "/**"
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/images/(.*)',
-        headers: [
+    images: {
+        remotePatterns: [
           {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable',
+            hostname: "res.cloudinary.com",
           },
         ],
       },
-    ];
-  },
+    async headers() {
+        return [
+            {
+                source: '/images/(.*)',
+                headers: [
+                    { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
+                ],
+            },
+        ];
+    },
 };
