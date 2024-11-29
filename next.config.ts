f89764cf1fec22dabe86ee3df8ab1f9cf6e44eb1
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-29 14:16:00 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ module.exports = {
     async headers() {
         return [
             {
-              source: '/(.*)\\.(jpg|jpeg|png|gif|webp|svg)',
+              source: '/(.*)\\.(jpg|jpeg|png|gif|webp|svg|bmp|tiff|ico|avif|heif)',
               headers: [
                     { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
                 ],
