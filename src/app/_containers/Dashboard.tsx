bd1f79dc43bb6763e76121bc053d494b05e9b406
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-11-30 00:53:56 +0100

    Update Dashboard.tsx

--- src/app/_containers/Dashboard.tsx
+++ src/app/_containers/Dashboard.tsx
@@ -15,41 +15,21 @@ interface RecipeCardProps {
 export default function Dashboard({ serverRecipes }: { serverRecipes: RecipePopulatedProps[] }) {
   const [totalColumns, setTotalColumns] = useState<number>(5);
   const [columns, setColumns] = useState<RecipeCardProps[][]>();
-  const [isFreshLoad, setIsFreshLoad] = useState<boolean>(false);
 
   useLayoutEffect(() => {
     setTotalColumns(window.innerWidth > 768 ? 5 : 3);
   }, [])
 
   useEffect(() => {
-    // On initial load (full-page reload)
-    if (window.performance && performance.navigation.type === 1) {
-      setIsFreshLoad(true); // Full page reload detected
-    } else {
-      setIsFreshLoad(false); // Normal navigation (using Next.js routing)
-    }
+    const fetchRecipes = async () => {
+      if (serverRecipes) {
+        const masonaryColumns = await createMasonary(serverRecipes, totalColumns);
+        setColumns(masonaryColumns);
+      }
+    };
+    fetchRecipes();
   }, []);
 
-  // Fetch recipes from server or use cached data (if available)
-  useEffect(() => {
-    const storedColumns = localStorage.getItem('columns');
-    if (!isFreshLoad && storedColumns) {
-      // Skip fetching from server and use cached columns
-      setColumns(JSON.parse(storedColumns));
-    } else {
-      // Fetch recipes from the server and cache them
-      const fetchRecipes = async () => {
-        if (serverRecipes) {
-          const masonaryColumns = await createMasonary(serverRecipes, totalColumns);
-          setColumns(masonaryColumns);
-          if (!isFreshLoad) {
-            localStorage.setItem('columns', JSON.stringify(masonaryColumns)); // Cache columns for future navigation
-          }
-        }
-      };
-      fetchRecipes();
-    }
-  }, [serverRecipes, totalColumns, isFreshLoad]); // Refetch when dependencies change
   if (!columns)
     return null
 
