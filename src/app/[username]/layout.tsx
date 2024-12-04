3865a1591c1f28c66ddbbb4fd3aeebeb97aefa8d
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-12-04 18:35:58 +0100

    s

--- src/app/[username]/layout.tsx
+++ src/app/[username]/layout.tsx
@@ -1,7 +1,7 @@
 import MasonaryProfile from "./_containers/MasonaryProfile";
 import { ReactNode } from "react";
 import Profile from "./_containers/Profile";
-import { getProfileController } from "./_ssr/profile/profileController";
+import { getProfileController } from "./_ssr/profileController";
 import { ProfileProvider } from "../_context/ProfileContext";
 import ErrorPage from "../_errors/ErrorPage";
 
