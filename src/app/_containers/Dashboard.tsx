0dfa6a1db8b05e5bbb3f6fe7e584dac3f8222e00
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-12-05 01:52:54 +0100

    Update Dashboard.tsx

--- src/app/_containers/Dashboard.tsx
+++ src/app/_containers/Dashboard.tsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from "react";
 import { fetchRecipesAPI } from "../_services/api/fetchRecipesAPI";
 import { RecipePopulatedProps } from "@/_models/RecipeModel";
 import { MasonryComponent } from "../_components/MasonryComponent";
-import { LoadingSpinner } from "../_components/LoadingSpinner";
 
 
 export default function Dashboard({ serverRecipes }: { serverRecipes?: RecipePopulatedProps[] }) {
   const [recipes, setRecipes] = useState<RecipePopulatedProps[]>( serverRecipes)
 
+<<<<<<< HEAD
   useEffect(() => {
     const fetchRecipes = async () => {
       const { fetchedRecipes } = await fetchRecipesAPI();
@@ -19,9 +19,24 @@ export default function Dashboard({ serverRecipes }: { serverRecipes?: RecipePop
     };
     fetchRecipes();
   }, [])
+=======
+  useEffect(()=>{
+    if (!recipes) {
+      const fetchRecipes = async () => {
+        const { fetchedRecipes } = await fetchRecipesAPI();
+        setRecipes(fetchedRecipes);
+        if (fetchedRecipes) {
+          sessionStorage.setItem('recipes', JSON.stringify(fetchedRecipes));
+        }
+      };
+      fetchRecipes();
+    }
+  
+  },[])
+>>>>>>> parent of 2c421a4 (Update Dashboard.tsx)
 
   if (!recipes)
-    return <LoadingSpinner />
+    return null
 
   if (recipes.length === 0) {
     return (
