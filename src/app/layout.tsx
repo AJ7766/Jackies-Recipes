d0a4336565deee7d9aa27661ee53c1263af6df8d
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2024-12-13 16:55:19 +0100

    z

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,28 @@ import { getSession } from "@/_utils/session";
 import { getUserController } from "./_ssr/user/userController";
 import { CacheProvider } from "./_context/CacheContext";
 import { NavBarWidth } from "./_services/navBarServices";
+import { Source_Sans_3 } from "next/font/google";
+import localFont from "next/font/local";
+
+const metropolis = localFont({
+  src: [
+    {
+      path: "/fonts/Metropolis-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-metropolis",
+  display: "swap",
+});
+
+
+const sourceSans = Source_Sans_3({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "600"],
+  variable: "--font-source-sans",
+});
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const session = await getSession();
@@ -26,7 +48,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+3:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <body className={sourceSans.className}>
         <AuthProvider serverUser={typeof serverUser === 'string' ? JSON.parse(serverUser) : serverUser}>
           <CacheProvider>
             <NavBar isAuth={session.isAuth} />
