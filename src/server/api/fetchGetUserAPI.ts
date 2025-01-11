ffee9c34636ab6aff2315daf49922794ab49ec6d
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-01-11 13:07:36 +0100

    Update fetchGetUserAPI.ts

--- src/server/api/fetchGetUserAPI.ts
+++ src/server/api/fetchGetUserAPI.ts
@@ -1,11 +1,9 @@
 "use server"
-import { getSession } from "@/_utils/session";
 import { getUserController } from "../actions/user/userController";
 
 export const fetchGetUserAPI = async () => {
     try {
-        const { token } = await getSession();
-        const user = await getUserController(token);
+        const user = await getUserController();
 
         return { message: "Fetch user successfully", fetchedUser: user };
     } catch (error) {
