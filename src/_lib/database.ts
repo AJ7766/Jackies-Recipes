89d1b7832ea66555e3eb7340687e9150def73f25
Jackie Huynh <39444662+AJ7766@users.noreply.github.com>
2025-02-21 22:57:31 +0100

    Update database.ts

--- src/_lib/database.ts
+++ src/_lib/database.ts
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 
 export const getUri = async () => {
    const MONGODB_URI_EXISTS = process.env.MONGODB_URI;
-   if (!MONGODB_URI_EXISTS) 
+   if (!MONGODB_URI_EXISTS)
       throw new Error('No MongoDB Uri found');
-   
+
    return MONGODB_URI_EXISTS
 }
 
@@ -16,15 +16,8 @@ export async function connectDB() {
    if (cachedClient || mongoose.connection.readyState === 1)
       return cachedClient;
 
-   try {
-      const MONGODB_URI = await getUri();
-      const client = await mongoose.connect(MONGODB_URI,{
-         serverSelectionTimeoutMS: 20000,
-      });      
-      cachedClient = client;
-      return client;
-   } catch (error: any) {
-      throw new Error(error.message);
-   }
-
+   const MONGODB_URI = await getUri();
+   const client = await mongoose.connect(MONGODB_URI);
+   cachedClient = client;
+   return client;
 }
